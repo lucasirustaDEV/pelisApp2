09d9549f3111b21ad592e4cd328e3e4c79428735
lucasirustaDEV <lucasirusta@gmail.com>
2023-09-25 14:17:53 -0300

    :tada: First commit pelisApp

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app-layout">
+      <header className="app-header">pelisApp Header</header>
+      <main className="app-main">pelisApp Main</main>
+      <footer className="app-footer">pelisApp Footer</footer>
+    </div>
+  )
+}
+
+export default App
